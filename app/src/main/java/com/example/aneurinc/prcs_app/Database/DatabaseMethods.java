3ce6d987af4ee1b66e60c3b8d1e6751408712977
Joshua Kellaway <dennis358.jk@gmail.com>
2016-03-14 18:37:12 +0000

    Added Database Classes

---
+++ app/src/main/java/com/example/aneurinc/prcs_app/Database/DatabaseMethods.java
@@ -0,0 +1,9 @@
+package com.example.aneurinc.prcs_app.Database;
+
+/**
+ * Created by Joshua Kellaway on 14/03/2016.
+ * This class uses the ApiConnection class and allows the rest of the application to access the database.
+ */
+public class DatabaseMethods {
+
+}
