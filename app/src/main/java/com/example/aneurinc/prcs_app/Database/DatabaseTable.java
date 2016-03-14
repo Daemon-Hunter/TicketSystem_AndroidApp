e39b9b7277c74fa333e80529f356d39005dcc1be
Joshua Kellaway <dennis358.jk@gmail.com>
2016-03-14 17:43:07 +0000

    Added Database Classes

---
+++ app/src/main/java/com/example/aneurinc/prcs_app/Database/DatabaseTable.java
@@ -0,0 +1,21 @@
+package com.example.aneurinc.prcs_app.Database;
+
+/**
+ * Created by Joshua Kellaway on 14/03/2016.
+ * Database table enum
+ */
+public enum DatabaseTable {
+    ARTISTS,
+    ARTISTREVIEWS,
+    BOOKINGS,
+    CHILDEVENTS,
+    CUSTOMERS,
+    EVENTREVIEWS,
+    GUESTBOOKINGS,
+    LINEUPS,
+    PARENTEVENTS,
+    SOCIALMEDIAS,
+    TICKETS,
+    VENUES,
+    VENUEREVIEWS
+}
