5b1db2fde934e99eba3d8a2f150342e43dc186b6
nsc900 <nsc900@hotmail.com>
2016-04-05 00:13:56 +0100

    Added custom location to map activity.

--- app/src/main/java/com/example/aneurinc/prcs_app/UI/MapActivity.java
+++ app/src/main/java/com/example/aneurinc/prcs_app/UI/MapActivity.java
@@ -1,10 +1,11 @@
 package com.example.aneurinc.prcs_app.UI;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,18 +19,56 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    private static final float ZOOM_LOCATION = 17.0f;
+    private final float ZOOM_VAL = 15.0f;
+    private LatLng location;
+    private String strAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
         setUpToolbar();
 
+        strAddress = "Plymouth University, Drake Circus, Plymouth PL4 8AA";
+
+        location = getLocationFromAddress();
+
+        initMapFragment();
+
+        TextView tvAddress = (TextView) findViewById(R.id.address);
+        tvAddress.setText(strAddress);
+    }
+
+    private LatLng getLocationFromAddress() {
+
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+
+        return p1;
+    }
+
+    private void initMapFragment() {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -56,6 +95,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         menu.findItem(R.id.tb_search).setVisible(false);
         return true;
@@ -78,11 +118,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        LatLng plymouth = new LatLng(50.3741, -4.1385);
-        mMap.addMarker(new MarkerOptions().position(plymouth).title("Event Title").snippet("Address goes here..."));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(plymouth, ZOOM_LOCATION));
+        // Add a marker and move the camera
+        googleMap.addMarker(new MarkerOptions().position(location).title("Location Address:").snippet(strAddress));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, ZOOM_VAL));
     }
-
 }
